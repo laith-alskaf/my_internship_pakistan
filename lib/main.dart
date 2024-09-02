7f163be707a0c5f689e77f04c6ea21508de132cd
laith-alskaf <liethmhmad231@gmail.com>
2024-09-02 21:29:11 +0300

    z

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:internship_pakistan/app/my_app.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(const MyApp());
 }
-
-
